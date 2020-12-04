302362766e238f4dffe8e71373ae78c5308f181e
treeform <starplant@gmail.com>
2020-12-03 21:37:03 -0800

    more files.

---
+++ benchy.nimble
@@ -0,0 +1,8 @@
+version     = "0.0.1"
+author      = "Andre von Houck"
+description = "Benchmarking"
+license     = "MIT"
+
+srcDir = "src"
+
+requires "nim >= 1.2.2"
---
+++ src/benchy.nim
@@ -0,0 +1,90 @@
+## Public interface to you library.
+
+import std/monotimes, strformat, math, strutils
+
+proc nowMs(): float64 =
+  getMonoTime().ticks.float64 / 1000000.0
+
+proc total(s: seq[SomeNumber]): float =
+  ## Computes total of a sequence.
+  for v in s:
+    result += v.float
+
+proc mean(s: seq[SomeNumber]): float =
+  ## Computes mean (average) of a sequence.
+  if s.len == 0: return NaN
+  s.total / s.len.float
+
+proc variance(s: seq[SomeNumber]): float =
+  ## Computes the sample variance of a sequence.
+  if s.len <= 1:
+    return
+  let a = s.mean()
+  for v in s:
+    result += (v.float - a) ^ 2
+  result /= (s.len.float - 1)
+
+proc stdev(s: seq[SomeNumber]): float =
+  ## Computes the sample standard deviation of a sequence.
+  sqrt(s.variance)
+
+proc removeOutliers(s: var seq[SomeNumber]) =
+  let avg = mean(s)
+  let std = stdev(s)
+  var i = 0
+  while i < s.len:
+    if abs(s[i] - avg) > std*2:
+      #echo "remove: ", s[i]
+      s.delete(i)
+      continue
+    inc i
+
+var keepPlace: pointer
+template keep*(value: untyped) =
+  keepPlace = value.unsafeAddr
+
+template timeIt*(tag: string, body: untyped) =
+  ## Quick template to time an operation.
+
+  var
+    num = 0
+    total: float64
+    deltas: seq[float64]
+    stdd: seq[float64]
+
+  proc `test tag`() =
+    body
+
+  while true:
+    inc num
+    let start = nowMs()
+
+    `test tag`()
+
+    let finish = nowMs()
+
+    let delta = finish - start
+    total += delta
+    deltas.add(delta)
+    if total > 60_000.0:
+      break
+    stdd.add(stdev(deltas))
+    if num >= 10:
+      if mean(stdd[^9 .. ^1]) - mean(stdd[^5 .. ^1]) < 0.2:
+        break
+
+  removeOutliers(deltas)
+  removeOutliers(deltas)
+
+  var readout = ""
+  var s = ""
+  var d = ""
+  formatValue(s, mean(deltas) , "0.3f")
+  formatValue(d, stdev(deltas) , "0.3f")
+  readout = s & " ms " & align("±" & d,10) & "  x" & $num
+
+  var dots = ""
+  for i in tag.len + s.len .. 40:
+    dots.add(".")
+
+  echo tag, " ", dots, " ", readout

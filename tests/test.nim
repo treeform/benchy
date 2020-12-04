f35b25bc93c1bfc8e39c0688382b4a610462d45f
treeform <starplant@gmail.com>
2020-12-03 21:36:25 -0800

    first commit

--- tests/test.nim
+++ tests/test.nim
@@ -1,3 +1,24 @@
 ## Put your tests here.
 
-import nimtemplate
+import benchy, os, random
+
+timeIt "sleep 1ms":
+  sleep(1)
+
+timeIt "sleep 200ms":
+  sleep(200)
+
+timeIt "sleep randms":
+  sleep(rand(0 .. 150))
+
+timeIt "number counter":
+  var s = 0
+  for i in 0 .. 1_000_000:
+    s += s
+  keep(s)
+
+timeIt "string append":
+  var s = "?"
+  for i in 0 .. 26:
+    s.add(s)
+  keep(s)
